6239f1c6a6af13b5b71507598cebdc1b779e1c71
patilmahesh007 <patilmahesh4007>
2024-08-06 22:13:07 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"
 
-const root = createRoot(document.getElementById('root'))
+const root= createRoot(document.getElementById('root'))
 
 
-const Mahesh = (props) => {
-    return <h1>Hello I am {props.name} I study in {props.class} </h1>
+const Mahesh=({name,city})=>{
+    return <h1>Hi I am {name} i am from {city}</h1>
 }
 root.render(
-    <>     <Mahesh name="Mahesh" class="FYMCS" />
-           <Mahesh name="Aditya" class="TYBCS" />
-          <Mahesh name="Sarthak" class="SYBCS" />
-    </>
-) 
+<>
+<Mahesh name="Mahesh" city=" pune"/>
+<Mahesh name="Yash" city=" pune"/>
+<Mahesh name="Aditya" city=" Mumbai"/>
+</>
+)
